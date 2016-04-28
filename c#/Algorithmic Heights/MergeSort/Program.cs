a4d321fec4ea63677884b507c53e79def0a67167
TravisAvey <avey.travis@gmail.com>
2016-04-28 18:59:36 -0500

    Done with Merge Sort in C#

--- c#/Algorithmic Heights/MergeSort/Program.cs
+++ c#/Algorithmic Heights/MergeSort/Program.cs
@@ -6,7 +6,6 @@ namespace MergeSort
 	class Merge
 	{
 		int [] A;
-		int [] b;
 		int size;
 
 		public Merge(int[] A, int size)
@@ -35,7 +34,42 @@ namespace MergeSort
 
 		private void merge(int lo, int mid, int hi)
 		{
+			int[] b = new int[size];
+			for (int n = 0; n < size; ++n)
+				b [n] = A [n];
 
+			int i = lo;
+			int j = mid + 1;
+			int k = lo;
+
+			// while the lo is less than mid point
+			// and mid+1 is less than hi point
+			while(i <= mid && j <= hi)
+			{
+				// if the b[i] is less than the b[j]
+				// that means, the i-th element is
+				// to be copied over to that i pos
+				// else they are out of order,
+				// copy the jth pos
+				if (b[i] <= b[j])
+					A[k++] = b[i++];
+				else
+					A[k++] = b[j++];
+			}
+
+			// copy b -> A, in correct order
+			// after the other elements are already
+			// copied over from above while loop
+			while (i <= mid)
+				A[k++] = b[i++];
+			while (j <= hi)
+				A[k++] = b[j++];
+		}
+
+		public void Print()
+		{
+			foreach (int n in A)
+				Console.Write ("{0} ", n);
 		}
 	}
 
@@ -57,6 +91,7 @@ namespace MergeSort
 
 				Merge merge = new Merge (a, n);
 				merge.MergeSort ();
+				merge.Print ();
 			}
 		}
 	}
