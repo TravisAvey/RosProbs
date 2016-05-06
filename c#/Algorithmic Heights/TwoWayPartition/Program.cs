e3cb41be6a32946d1426b34d30bb3c2bfaa7e44f
TravisAvey <avey.travis@gmail.com>
2016-05-06 09:57:17 -0500

    Done with 2 way partition

--- c#/Algorithmic Heights/TwoWayPartition/Program.cs
+++ c#/Algorithmic Heights/TwoWayPartition/Program.cs
@@ -17,37 +17,74 @@ namespace TwoWayPartition
 {
 	class MainClass
 	{
-		public static void Partition(int[] A, int l, int r)
+		/*
+		 * 	The sub routine of Quicksort.  This is a 2 way
+		 * 	partition.
+		 */ 
+		public static void Partition(int[] A, int lo, int hi)
 		{
-			int p = A [l];
-			int i = l + 1;
+			// variables for positions
+			int i = lo;
+			int j = hi + 1;
+			// var for pivot point
+			int v = A [lo];
 
-			for (int j=i; j<r; ++j)
+			while (true)
 			{
-				if (A[j] < p)
-				{
-					Swap (A, j, i);
-					i++;
-				}
+				// check values for lower than pivot
+				while (A[++i] <= v)
+					if (i == hi)
+						break;
+
+				// checking for values higher than pivot
+				while (v < A[--j])
+					if (j == lo)
+						break;
+
+				// once we are here, i passes j, break out
+				if (i >= j)
+					break;
+
+				/*
+				 * 	From the 2 while loops when we get
+				 * 	here, the two values need to be swapped
+				 */ 
+				Swap (A, i, j);
 			}
 
-			Swap (A, l, i-1);
+			// swap the pivot to correct position
+			Swap (A, lo, j);
 
+			// save data to file
 			PrintToFile (ref A);
 		}
 
+		/*
+		 * 	helper method to print data to a file for easier
+		 * 	answer upload
+		 */ 
 		private static void PrintToFile(ref int [] A)
 		{
+			// open a file stream with access of write
 			var file = new FileStream (@"/home/travis/GitHub/RosProbs/c#/Algorithmic Heights/TwoWayPartition/output.txt",
 			                          FileMode.Open, FileAccess.Write);
+
+			// using a stream writer on the file stream
 			using (var writer = new StreamWriter(file)) 
 			{
+				// write each value to the file
 				foreach (int a in A)
 					writer.Write("{0} ", a);
 			}
 		}
+
+		/*
+		 * 	Helper method to swap 2 values in passed in
+		 * 	array.  The values are the indices of array
+		 */ 
 		private static void Swap(int[] a, int i, int j)
 		{
+			// perform swap
 			int swap = a [i];
 			a [i] = a [j];
 			a [j] = swap;
@@ -55,17 +92,22 @@ namespace TwoWayPartition
 
 		public static void Main (string[] args)
 		{
-			var file = new FileStream (@"/home/travis/GitHub/RosProbs/c#/Algorithmic Heights/TwoWayPartition/data.txt",
+			// open new file stream on file
+			var file = new FileStream (@"/home/travis/GitHub/RosProbs/c#/Algorithmic Heights/TwoWayPartition/rosalind_par.txt",
 			                          FileMode.Open, FileAccess.Read);
 
+			// using a stream reader of the file stream
 			using (var reader = new StreamReader(file))
 			{
+				// get the size of the array
 				int size = int.Parse(reader.ReadLine ());
+				// get the next line, which is the entire array
 				string line = reader.ReadLine ();
+				// convert the line into 1st a list, then convert to an array
 				int[] A = new List<int> (Array.ConvertAll(line.Split(' '), int.Parse)).ToArray();
 
+				// call sub routine to partition array
 				Partition (A, 0, size-1);
-
 			}
 		}
 	}
