385dcaaaccea194ab896c1ee266ddde500e2047f
TravisAvey <avey.travis@gmail.com>
2016-05-10 10:32:00 -0500

    Got all probabilities of picking two randoms, next find probability of each to get an individual possessing a dominant allele

--- c#/Bioinformatic Strongholds/MendelsFirstLaw/Program.cs
+++ c#/Bioinformatic Strongholds/MendelsFirstLaw/Program.cs
@@ -10,6 +10,33 @@ namespace MendelsFirstLaw
 		{
 			return (AA / total) * (AA - 1) / (total - 1);
 		}
+
+		public static double AAandAa(double AA, double Aa, double total)
+		{
+			return 2 * (AA / total) * (Aa / (total - 1));
+		}
+
+		public static double AAandaa(double AA, double aa, double total)
+		{
+			return 2 * (AA / total) * (aa / (total - 1));
+		}
+
+		public static double AaandAa(double Aa, double total)
+		{
+			return (Aa / total) * (Aa - 1) / (total - 1);
+		}
+
+		public static double Aaandaa(double Aa, double aa, double total)
+		{
+			return 2 * (Aa / total) * (aa / (total - 1));
+		}
+
+		public static double aaandaa(double aa, double total)
+		{
+			return (aa / total) * ((aa - 1) / (total - 1));
+		}
+
+
 		public static void Main (string[] args)
 		{
 			var file = new FileStream (@"/home/travis/GitHub/RosProbs/c#/Bioinformatic Strongholds/MendelsFirstLaw/data.txt",
@@ -26,7 +53,25 @@ namespace MendelsFirstLaw
 				double total = numAA + numAa + numaa;
 
 				double probAA_AA = AAandAA(numAA, total);
-				Console.WriteLine (probAA_AA);
+				Console.WriteLine ("Probability of AA and AA {0}", probAA_AA);
+
+				double probAA_Aa = AAandAa (numAA, numAa, total);
+				Console.WriteLine ("Probability of AA and Aa {0}", probAA_Aa);
+
+				double probAA_aa = AAandaa (numAA, numaa, total);
+				Console.WriteLine ("Probability of AA and aa {0}", probAA_aa);
+
+				double probAa_Aa = AaandAa (numAa, total);
+				Console.WriteLine ("Probability of Aa and Aa {0}", probAa_Aa);
+
+				double probAa_aa = Aaandaa (numAa, numaa, total);
+				Console.WriteLine ("Probability of AA and aa {0}", probAa_aa);
+
+				double probaa_aa = aaandaa (numaa, total);
+				Console.WriteLine ("Probability of aa and aa {0}", probaa_aa);
+
+				double tot = probAA_AA + probAA_Aa + probAA_aa + probAa_Aa + probAa_aa + probaa_aa;
+				Console.WriteLine ("Total probabilty {0}", tot);
 			}
 		}
 	}
