b39b14bd0d4609c305a00664fc5c4e5bdb908118
TravisAvey <avey.travis@gmail.com>
2016-05-16 10:01:34 -0500

    Getting Max Heap working.  My heap not matching test data yet.

--- c#/Algorithmic Heights/BuildHeap/Program.cs
+++ c#/Algorithmic Heights/BuildHeap/Program.cs
@@ -6,6 +6,65 @@ namespace BuildHeap
 {
 	class MainClass
 	{
+		public class MaxHeap
+		{
+			private int size;
+			private int[] mHeap;
+			private int pos;
+
+			public MaxHeap(int size)
+			{
+				this.size = size;
+				mHeap = new int[size+1];
+				mHeap[0] = int.MaxValue;
+				pos = 0;
+			}
+
+			public void CreateHeap(int [] A)
+			{
+				for (int i=0; i<size; ++i)
+				{
+					Insert (A [i]);
+				}
+			}
+
+			public void DisplayHeap()
+			{
+				for (int i=1; i<=size; i++)
+					Console.Write ("{0} ", mHeap [i]);
+				Console.WriteLine ();
+			}
+
+			private void Insert(int n)
+			{
+				mHeap [++pos] = n;
+				int current = pos;
+
+				while (mHeap[current] > mHeap[Parent(current)])
+				{
+					Swap (current, Parent (current));
+					current = Parent (current);
+				}
+			}
+
+			private void BubbleUp()
+			{
+
+			}
+
+			private void Swap(int i, int j)
+			{
+				int temp = mHeap [i];
+				mHeap [i] = mHeap [j];
+				mHeap [j] = temp;
+			}
+
+			private int Parent(int i) { return i / 2; }
+
+			private int RightChild(int i) { return 2 * i; }
+
+			private int LeftChild(int i) { return (2 * i) + 1; }
+		}
 		public static void Main (string[] args)
 		{
 			var file = new FileStream (@"/home/travis/GitHub/RosProbs/c#/Algorithmic Heights/BuildHeap/data.txt",
@@ -17,6 +76,11 @@ namespace BuildHeap
 
 				string line = reader.ReadLine ();
 				int[] tokens = new List<int> (Array.ConvertAll (line.Split (' '), int.Parse)).ToArray ();
+
+				MaxHeap heap = new MaxHeap (nodes);
+				heap.CreateHeap (tokens);
+				heap.DisplayHeap ();
+
 			}
 		}
 	}
